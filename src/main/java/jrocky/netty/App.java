985ac4a72c3f06a853181098b2c6a3f4e9ab25dd
wangzhijie <wangzhijie@b.360.cn>
2018-02-23 10:33:26 +0800

    init

---
+++ src/main/java/jrocky/netty/App.java
@@ -0,0 +1,13 @@
+package jrocky.netty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
